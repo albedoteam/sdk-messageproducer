002af3415bb462a7ffc38dde6d00209146b533c8
fiduta <eryckson@gmail.com>
2020-09-19 13:21:04 -0300

    init

--- MessageProducerSdk/MessageProducerSdk/Configuration/Abstractions/IProducerOptions.cs
+++
@@ -1,7 +0,0 @@
-namespace MessageProducerSdk.Configuration.Abstractions
-{
-    public interface IProducerOptions
-    {
-        public string BrokerHost { get; set; }
-    }
-}
--- MessageProducerSdk/MessageProducerSdk/Configuration/ProducerOptions.cs
+++
@@ -1,9 +0,0 @@
-using MessageProducerSdk.Configuration.Abstractions;
-
-namespace MessageProducerSdk.Configuration
-{
-    public class ProducerOptions : IProducerOptions
-    {
-        public string BrokerHost { get; set; }
-    }
-}
